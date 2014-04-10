88d6c1bb64975147c438bb31c59165fee3647abd
Egbert Veenstra <egbertveenstra@gmail.com>
2014-04-10 10:19:21 +0200

    filter ENV variables rake exceptions

--- templates/airbrake.rb
+++ templates/airbrake.rb
@@ -4,4 +4,7 @@ Airbrake.configure do |config|
   config.port    = 443
   config.secure  = true
   config.rescue_rake_exceptions = true
+  ENV.keys.each do |filtered_key|
+    config.rake_environment_filters << filtered_key
+  end
 end
