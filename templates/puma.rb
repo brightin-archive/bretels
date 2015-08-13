a4e6ab99b8a81b7a057c663510c726ccc047540c
Stefan Roex <info@stefanroex.nl>
2015-08-13 09:47:48 +0200

    Use puma configuration as suggested by Heroku. closes #26
    For more information see:
    https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#config

--- templates/puma.rb
+++ templates/puma.rb
@@ -1,17 +1,13 @@
-workers Integer(ENV["WEB_CONCURRENCY"] || 3)
-threads Integer(ENV["MIN_THREADS"] || 1), Integer(ENV["MAX_THREADS"] || 16)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 3)
+threads threads_count, threads_count
 
 preload_app!
 
-rackup DefaultRackup
-port ENV["PORT"] || 3000
-environment ENV["RACK_ENV"] || "development"
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  ActiveSupport.on_load(:active_record) do
-    config = ActiveRecord::Base.configurations[Rails.env] ||
-             Rails.application.config.database_configuration[Rails.env]
-    config["pool"] = ENV["MAX_THREADS"] || 16
-    ActiveRecord::Base.establish_connection(config)
-  end
+  ActiveRecord::Base.establish_connection
 end
